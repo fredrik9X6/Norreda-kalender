1b4caff62dd8a40f9e1b4197291763153cf5a507
fredrik9X6 <fredrik@9x6.se>
2023-06-10 16:28:22 +0200

    adding price calculator

--- src/index.ts
+++ src/index.ts
@@ -40,3 +40,76 @@ const getEvents = (): Event[] => {
 
   return events;
 };
+
+const rates = {
+  Sportstugan: { weekday: 100, weekend: 120 },
+  Ladan: { weekday: 80, weekend: 100 },
+  Torpet: { weekday: 90, weekend: 110 },
+  'Visthuset nedre': { weekday: 110, weekend: 130 },
+  'Visthuset övre': { weekday: 110, weekend: 130 },
+  Sovstugan: { weekday: 70, weekend: 90 },
+  Bastun: { weekday: 60, weekend: 80 },
+};
+
+// Update the total price based on the form inputs
+function updateTotalPrice() {
+  const startDateInput = document.querySelector('input[data-custom="start-date"]');
+  const endDateInput = document.querySelector('input[data-custom="end-date"]');
+  const selectedLodges = document.querySelectorAll('input[data-custom="lodge"]:checked');
+  const isMember = document.querySelector<HTMLFormElement>(
+    'input[data-custom="member-checkbox"]'
+  ).checked;
+
+  // Convert start and end dates to dd-mm-yyyy format
+  const startDate = formatDate(startDateInput.value);
+  const endDate = formatDate(endDateInput.value);
+
+  let totalPrice = 0;
+
+  // Calculate the total price based on selected lodges and date range
+  const currentDate = new Date(startDate.year, startDate.month - 1, startDate.day);
+
+  while (currentDate <= new Date(endDate.year, endDate.month - 1, endDate.day)) {
+    const dayOfWeek = currentDate.getDay();
+
+    selectedLodges.forEach(function (checkbox) {
+      const lodge = checkbox.getAttribute('data-lodge-name');
+      const rate = dayOfWeek === 0 || dayOfWeek === 6 ? rates[lodge].weekend : rates[lodge].weekday;
+      totalPrice += rate;
+    });
+
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  // Apply member discount if applicable
+  if (isMember) {
+    totalPrice *= 0.8; // 20% discount for members
+  }
+
+  document.getElementById('total-price').textContent = totalPrice.toFixed(0) + ' kr'; // Display total price with 0 decimal places
+}
+
+// Helper function to format date as dd-mm-yyyy
+function formatDate(dateString) {
+  const dateParts = dateString.split('-');
+  return {
+    day: dateParts[2],
+    month: dateParts[1],
+    year: dateParts[0],
+  };
+}
+
+// Add event listeners to form inputs
+document
+  .querySelector('input[data-custom="start-date"]')
+  .addEventListener('change', updateTotalPrice);
+document
+  .querySelector('input[data-custom="end-date"]')
+  .addEventListener('change', updateTotalPrice);
+const lodgeCheckboxes = document.querySelectorAll('input[data-custom="lodge"]');
+lodgeCheckboxes.forEach(function (checkbox) {
+  checkbox.addEventListener('change', updateTotalPrice);
+});
+document
+  .querySelector('input[data-custom="member-checkbox"]')
+  .addEventListener('change', updateTotalPrice);
